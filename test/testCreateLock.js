9e0247d87309d4c2b89eff9bd036b329753f7acc
opcc <opcc22059@gmail.com>
2022-02-09 19:52:21 +0800

    update test

--- test/testCreateLock.js
+++ test/testCreateLock.js
@@ -30,7 +30,14 @@ describe("test uniswap price oracle", function () {
         
         const veiZiFactory = await ethers.getContractFactory("VeiZi");
         const secondsPerBlockX64 = BigNumber(14).times(BigNumber(2).pow(64)).toFixed(0);
-        veiZi = await veiZiFactory.deploy(iZi.address, secondsPerBlockX64);
+        veiZi = await veiZiFactory.deploy(iZi.address, secondsPerBlockX64, {
+            provider: signer.address,
+            accRewardPerShare: 0,
+            rewardPerBlock: 0,
+            lastTouchBlock: 0,
+            startBlock: 0,
+            endBlock: 0
+        });
 
         iZi.connect(tester).approve(veiZi.address, '1000000000000000000000000000000');
         iZi.mint(tester.address, '1000000000000000000000000000000');
