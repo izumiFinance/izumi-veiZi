cba1f02c8e6439dce2e5eecff8892177edf6a3eb
opcc <opcc22059@gmail.com>
2022-03-03 15:17:32 +0800

    update

--- scripts/deployed.js
+++ scripts/deployed.js
@@ -2,7 +2,7 @@ const contracts = {
 	// tokens and contracts
     izumiTest: {
         iZi: "0xEe5e3852434eB67F8e9E97015e32845861ea15E8",
-        veiZi: '0x3f0e30cf2978f89fa5A8904efFb72bD085fA61E3',
+        veiZi: '0xbAcDb201B0e92ED97Bb7005A705C1CEf332175DC',
     },
 
 }
